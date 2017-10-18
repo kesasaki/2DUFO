4febd04d2aa97b60deb5511cf4e214a65e13f99a
Kei Sasaki <sasaki.kei@nitzlab.com>
2017-10-19 01:15:02 +0900

    + start scene

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+	public string scenename;
+
+	void Start () {
+		GetComponent<Button>().onClick.AddListener(OnClick);
+	}
+
+	void Update () {
+	}
+
+	public void OnClick() {
+		Debug.Log (scenename);
+		SceneManager.LoadScene(scenename);
+	}
+}
