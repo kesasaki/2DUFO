76ef0fd5e8667a6d2f4c47c511e2a7873e829207
Kei Sasaki <sasaki.kei@nitzlab.com>
2017-10-20 01:11:19 +0900

    自機弾の実装、敵に当てたらwin表示

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,24 +6,26 @@ using UnityEngine.UI;
 public class BulletController : MonoBehaviour {
 
 	public int damage;
-	public Vector2 movement;
 	public bool enemybullet;
-
-
-	private int count;
-	private Rigidbody2D rb2d;
+	public int range = 100;
+	private int distance = 0;
 
 	void Start() {
-		rb2d = GetComponent<Rigidbody2D> ();
 	}
 
-	void FixedUpdate () {
-		rb2d.MovePosition (rb2d.position + movement);
+	void Update() {
+		if (distance > range) {
+			Destroy (this.gameObject);
+		}
+		distance += 1;
 	}
 
 	// 衝突
 	void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log ("aaaa");
+		if ((enemybullet && other.tag == "Player") || (!enemybullet && other.tag == "Enemy")) {
+			Debug.Log (other.tag + " " + enemybullet);
+			other.gameObject.SendMessage ("damage", damage);
+			Destroy (this.gameObject);
+		}
 	}
-
 }
--- Assets/Scripts/SpiralWeapon.cs
+++ Assets/Scripts/SpiralWeapon.cs
@@ -8,11 +8,17 @@ public class SpiralWeapon : MonoBehaviour {
 	public float speed;
 	public Vector2 angle;
 	public float angleRate;
+	public int damage;
+	public int range;
 	public float bulletsize;
 	public bool clockwise;
 	private float timeElapsed = 0f;
+	private bool enemybullet = true;
 
 	void Start () {	
+		if (tag == "Player") {
+			enemybullet = false;
+		}
 	}
 
 	void Update () {
@@ -20,6 +26,9 @@ public class SpiralWeapon : MonoBehaviour {
 		if(timeElapsed >= interval) {
 			GameObject obj =  Instantiate (bullet, transform.position, Quaternion.identity);
 			obj.GetComponent<Rigidbody2D> ().velocity = angle * speed;
+			obj.GetComponent<BulletController>().enemybullet = enemybullet;
+			obj.GetComponent<BulletController>().damage = damage;
+			obj.GetComponent<BulletController>().range = range;
 			obj.transform.localScale *= bulletsize;
 			timeElapsed = 0.0f;
 			int clockwiseornot = (clockwise) ? -1 : 1;
