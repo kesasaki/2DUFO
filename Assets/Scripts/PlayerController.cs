f97f5bb3568ef491498f65fca0142fbff0ffe8be
Kei Sasaki <sasaki.kei@nitzlab.com>
2017-10-24 00:23:51 +0900

    + Scene.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
 	public int speedtilt;
 	public int speedkey;
-	public Text countText;
-	public Text winText;
 	public int hitpoint;
-	public string nextscene;
 	public AudioClip audio_damage;
 	public GameObject audio_explosion;
 	public GameObject end_perticle;
 
 	private AudioSource audioSource;
-	private int count;
 	private Rigidbody2D rb2d;
 
 	void Start() {
 		audioSource = gameObject.GetComponent<AudioSource> ();
 		rb2d = GetComponent<Rigidbody2D> ();
-		count = 0;
-		SetCountText ();
-		winText.text = "";
 	}
 
 	void FixedUpdate () {
@@ -48,22 +39,6 @@ public class PlayerController : MonoBehaviour {
 		}
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb2d.MovePosition (rb2d.position + movement);
-	}
-
-	// 衝突
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.CompareTag ("PickUp")) {
-			other.gameObject.SetActive(false);
-			count = count + 100;
-			SetCountText ();
-		}
-	}
-
-	void SetCountText() {
-		countText.text = "Score: " + count.ToString();
-		if (count >= 1200) {
-			winText.text = "YOU WIN";
-		}
 	}
 
 	void damage(int damage) {
---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour {
+
+	public Text countText;
+	public Text winText;
+	public string nextscene;
+
+	void Start () {
+		winText.text = "";
+		
+	}
+
+	void Update () {
+		int enemynum =  GameObject.FindGameObjectsWithTag("Enemy").Length;
+		int playernum = GameObject.FindGameObjectsWithTag("Player").Length;
+		countText.text = "Enemy: " + enemynum.ToString();
+		if (enemynum <= 0) {
+			winText.text = "YOU WIN";
+			Invoke("goNextScene", 3.5f);
+		}
+		if (playernum <= 0) {
+			winText.text = "YOU LOSE";
+			Invoke("goNextScene", 3.5f);
+		}
+	}
+
+	void goNextScene() {
+		SceneManager.LoadScene(nextscene);
+	}
+}
