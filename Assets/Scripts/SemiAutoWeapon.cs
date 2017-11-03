5ed74230147064beed3c41d2a2484d19e32b863e
Kei Sasaki <sasaki.kei@nitzlab.com>
2017-11-04 01:39:33 +0900

    Weaponのスーパークラス化

--- Assets/Scripts/SemiAutoWeapon.cs
+++ Assets/Scripts/SemiAutoWeapon.cs
@@ -2,42 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SemiAutoWeapon : MonoBehaviour {
-	public GameObject bullet;
-	public GameObject end_perticle;
-	public AudioClip audio_shoot;
-	public bool shootAudioOn = false;
-	public int damage = 20;
-	public int range = 10;
-	public float bulletsize = 1;
-	private bool enemybullet = true;
-	private AudioSource audioSource;
-
-	void Start () {
-		if (tag == "Player") {
-			enemybullet = false;
-		}
-		audioSource = gameObject.GetComponent<AudioSource> ();
-	}
+public class SemiAutoWeapon : Weapon {
 
 	void Update () {
 
-		// タッチされた
+		// タッチ
 		for (int i = 0; i < Input.touchCount; ++i) {
 			if (Input.GetTouch (i).phase == TouchPhase.Ended){
-				shoot ();
+				base.shoot ();
 			}
-		}
-	}
-
-	void shoot() {
-		GameObject obj = Instantiate (bullet, transform.position, Quaternion.identity);
-		obj.GetComponent<BulletController> ().enemybullet = enemybullet;
-		obj.GetComponent<BulletController> ().damage = damage;
-		obj.GetComponent<BulletController> ().range = range;
-		obj.transform.localScale *= bulletsize;
-		if (audio_shoot != null) {
-			audioSource.PlayOneShot (audio_shoot, 0.1f);
 		}
 	}
 }
--- Assets/Scripts/nWayWeapon.cs
+++ Assets/Scripts/nWayWeapon.cs
@@ -2,29 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class nWayWeapon : MonoBehaviour {
-	public GameObject bullet;
+public class nWayWeapon : Weapon {
+
 	public GameObject target;
-	public GameObject end_perticle;
 	public float interval = 0.2f;
-	public float speed = 30;
 	public float anglerange = 30;
 	public int n = 3;
-	public int damage = 20;
-	public int range = 100;
-	public float bulletsize = 1.0f;
-	public AudioClip audio_shoot;
 	private float timeElapsed = 0f;
-	private bool enemybullet = true;
-	private AudioSource audioSource;
-
-
-	void Start () {
-		if (tag == "Player") {
-			enemybullet = false;
-		}
-		audioSource = gameObject.GetComponent<AudioSource> ();
-	}
 
 	void Update () {
 		timeElapsed += Time.deltaTime;
@@ -33,31 +17,15 @@ public class nWayWeapon : MonoBehaviour {
 				Vector2 angle = target.transform.position - transform.position;
 				angle.Normalize ();
 				if (n == 1) {
-					GameObject obj = Instantiate (bullet, transform.position, Quaternion.identity);
+					GameObject obj = base.shoot ();
 					obj.GetComponent<Rigidbody2D> ().velocity = angle * speed;
-					obj.GetComponent<BulletController> ().enemybullet = enemybullet;
-					obj.GetComponent<BulletController> ().damage = damage;
-					obj.GetComponent<BulletController> ().range = range;
-					obj.GetComponent<BulletController> ().end_perticle = end_perticle;
-					obj.transform.localScale *= bulletsize;
 					timeElapsed = 0.0f;
-					if (audio_shoot != null) {
-						audioSource.PlayOneShot (audio_shoot, 0.1f);
-					}
 				} else {
 					for (int i = 0; i < n; i++) {
-						GameObject obj = Instantiate (bullet, transform.position, Quaternion.identity);
+						GameObject obj = base.shoot ();
 						Vector2 thisangle = Quaternion.Euler (0f, 0f, anglerange * ((float)i / (n - 1) - 0.5f)) * angle;
 						obj.GetComponent<Rigidbody2D> ().velocity = speed * thisangle;
-						obj.GetComponent<BulletController> ().enemybullet = enemybullet;
-						obj.GetComponent<BulletController> ().damage = damage;
-						obj.GetComponent<BulletController> ().range = range;
-						obj.GetComponent<BulletController> ().end_perticle = end_perticle;
-						obj.transform.localScale *= bulletsize;
 						timeElapsed = 0.0f;
-						if (audio_shoot != null) {
-							audioSource.PlayOneShot (audio_shoot, 0.1f);
-						}
 					}
 				}
 			}
