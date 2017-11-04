5ab7d59ce096322ec1ce0aa8b632f3879d138ad8
Kei Sasaki <sasaki.kei@nitzlab.com>
2017-11-05 00:03:36 +0900

    ロングタップでチャージ

--- Assets/Scripts/SemiAutoWeapon.cs
+++ Assets/Scripts/SemiAutoWeapon.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SemiAutoWeapon : Weapon {
+	public int expand_max = 40;
 	public AudioClip audio_shoot;
+
+	private float starttime = 0;
 
 	void Update () {
 
-		// タッチ
-		for (int i = 0; i < Input.touchCount; ++i) {
-			if (Input.GetTouch (i).phase == TouchPhase.Ended){
-				base.shoot ();
-				base.audioSource.PlayOneShot (audio_shoot, 1.0f);
-			}
+		// チャージ開始　タッチ開始 or space key ボタンダウン
+		if (Input.GetKeyDown (KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began)) {
+			starttime = Time.time;
+		}
+
+		// 弾を撃つ　タッチ終了 or space key ボタンアップ
+		if (Input.GetKeyUp (KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended)){
+			GameObject obj = base.shoot ();
+			obj.transform.parent = this.transform;
+			obj.GetComponent<BulletController> ().range = Mathf.Min(expand_max / 2, (int)((Time.time - starttime) * 100));
+			Debug.Log ((Time.time - starttime) * 100);
+			base.audioSource.PlayOneShot (audio_shoot, 1.0f);
 		}
 	}
 }
